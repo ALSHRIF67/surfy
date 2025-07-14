a2b44b3fc43af24231c5f20ad0bb150aa71885fd
ALSHRIF67 <mr048530@gmail.com>
2025-07-14 13:41:15 -0700

    Initial commit

---
+++ last/src/App.jsx
@@ -0,0 +1,69 @@
+export default function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-start bg-white px-4 sm:px-6 lg:px-8 ml-[397px]">
+      <div className="w-full max-w-md lg:max-w-lg space-y-8">
+        <div>
+          <img
+            className="mx-auto h-12 w-auto"
+            src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
+            alt="Your Company"
+          />
+          <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+            Sign in to your account
+          </h2>
+        </div>
+
+        <form className="space-y-6" method="POST">
+          <div>
+            <label htmlFor="email" className="block text-sm font-medium text-gray-900">
+              Email address
+            </label>
+            <input
+              id="email"
+              name="email"
+              type="email"
+              autoComplete="email"
+              required
+              className="mt-1 block w-full rounded-md border border-gray-300 px-3 py-2 text-gray-900 shadow-sm placeholder-gray-400 focus:border-indigo-600 focus:ring-indigo-600 sm:text-sm"
+            />
+          </div>
+
+          <div>
+            <div className="flex items-center justify-between">
+              <label htmlFor="password" className="block text-sm font-medium text-gray-900">
+                Password
+              </label>
+              <a href="#" className="text-sm font-semibold text-indigo-600 hover:text-indigo-500">
+                Forgot password?
+              </a>
+            </div>
+            <input
+              id="password"
+              name="password"
+              type="password"
+              autoComplete="current-password"
+              required
+              className="mt-1 block w-full rounded-md border border-gray-300 px-3 py-2 text-gray-900 shadow-sm placeholder-gray-400 focus:border-indigo-600 focus:ring-indigo-600 sm:text-sm"
+            />
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="flex w-full justify-center rounded-md bg-indigo-600 px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+            >
+              Sign in
+            </button>
+          </div>
+        </form>
+
+        <p className="text-center text-sm text-gray-500">
+          Not a member?{' '}
+          <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+            Start a 14 day free trial
+          </a>
+        </p>
+      </div>
+    </div>
+  );
+}
